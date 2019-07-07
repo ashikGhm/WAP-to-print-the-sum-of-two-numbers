58283bc65659dc29136ba01392ac16a469501bf7
Ashik Ghimire <52419856+ashikGhm@users.noreply.github.com>
2019-07-07 20:58:35 +0545

    Adding two numbrs

---
+++ Q_two.java
@@ -0,0 +1,23 @@
+package Assignment_unit1;
+
+/**
+ *Print the sum of two numbers.
+ */
+public class Q_two {
+
+    public static void main(String[]args){
+        //declare
+        int First_num,Second_num,result;
+        //assign the value
+        First_num=5;
+        Second_num=10;
+        //process
+        result=(First_num+Second_num);
+        //output
+        System.out.println(First_num+" + "+Second_num+" = "+result);
+
+
+
+    }
+
+}
